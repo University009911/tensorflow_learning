76e8d392f9c58b4c5221ca2d75109440d12c2d72
tao.yu <yu2259420@163.com>
2023-09-08 17:45:14 +0800

    up

--- Basics/tutorial3-neuralnetwork.py
+++ Basics/tutorial3-neuralnetwork.py
@@ -7,12 +7,52 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist
 
+
+def pp():
+    return print("####################################") 
 #physical_devices = tf.config.list_physical_devices("GPU")
 #tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# x_train里面每一个element都是二维数组
+print (x_train[5])
+print(len(x_train[5]))
+pp()
 x_train = x_train.reshape(-1, 28 * 28).astype("float32") / 255.0
 x_test = x_test.reshape(-1, 28 * 28).astype("float32") / 255.0
+
+print(x_train.shape)  #(60000, 28, 28, 1)
+print(x_train[0])    #输出第一个element
+print(x_train[0].shape)    #输出第一个element的维度
+
+"""
+###/reshape(-1, 28 * 28)解释
+在上面的代码中，reshape(-1, 28 * 28)是将输入的图像数据重新塑造成形状为(-1, 28 * 28)的矩阵的操作。具体来说，
+它将每张 28x28 的图像转换为一个 784 维的向量，其中 784 等于 28*28，即图像的像素数量。
+这样做的目的是将图像数据转换为适合训练神经网络的格式。神经网络通常需要输入一个向量，而不是一张图像，因此需要将图像数据转换为向量形式。
+此外，将图像数据转换为相同大小的向量也有助于在训练过程中规范化数据，从而提高模型的准确性和泛化能力。
+因此，代码中的reshape(-1, 28 * 28)语句将每张图像转换为一个 784 维的向量，以便后续训练神经网络。
+
+
+在`reshape(-1, 28 * 28)`这个操作中，-1 表示在矩阵的维度中，有一个维度是未知的，需要根据输入数据的大小来确定。
+具体来说，当使用`reshape`函数将一个矩阵重塑为另一个形状时，需要指定新矩阵的每一维的大小。但是，在这个操作中，有一个维度的大小被设置为-1，
+这意味着它的大小将根据输入数据的大小自动计算。在上面的代码中，由于输入的图像数据是一个二维数组，因此需要将其转换为一个一维向量。在这种情况下，
+使用`reshape(-1, 28 * 28)`操作可以将二维数组中的所有数据沿着一个维度展开，并将其转换为一个形状为(-1, 784)的一维向量。其中，-1 表示这个向量的长度是未知的，
+需要根据输入数据的大小来确定，而 784 则表示转换后的向量中每个元素的大小，即 28 * 28 = 784。
+因此，使用`reshape(-1, 28 * 28)`操作可以根据输入数据的大小自动计算新矩阵的大小，从而实现将图像数据转换为适合训练神经网络的格式的目的。
+"""
+
+
+"""
+### /255 解释
+在上面的代码中，除以 255 的操作是将图像数据归一化到范围[0, 1]内的过程。具体来说，原始的 MNist 数据集中的图像是以灰度格式存储的，
+每个像素的值范围是[0, 255]，其中 0 表示黑色，255 表示白色。为了将这些图像数据转换为适合训练神经网络的格式，需要将它们归一化到范围[0, 1]内。
+除以 255 的操作就是将每个像素的值除以 255，从而将其转换为浮点数，并且将其范围映射到[0, 1]之间。这样可以确保在训练神经网络时，
+图像数据具有相同的范围和尺度，从而提高模型的准确性和泛化能力。因此，代码中的
+x_train = x_train.reshape(-1, 28 * 28).astype("float32") / 255.0 和 x_test = x_test.reshape(-1, 28 * 28).astype("float32") / 255.0
+语句分别对训练集和测试集的图像数据进行了归一化操作。
+"""
+
 
 # Sequential API (Very convenient, not very flexible)
 model = keras.Sequential(
@@ -43,5 +83,5 @@ model.compile(
     metrics=["accuracy"],
 )
 
-model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
+model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)   #训练model  60000份数据，每个批次32个，所有数据总共1875份   整个数据集训练5次
 model.evaluate(x_test, y_test, batch_size=32, verbose=2)
---
+++ tensorflow_ipynb/toturial3.py
@@ -0,0 +1,102 @@
+"""
+https://www.52txr.cn/2022/metricslossacc.html    代码来源  可以跟着flow\tensorflow_learning\Basics\tutorial3-neuralnetwork.py比较着看
+"""
+
+
+import tensorflow as tf
+(train_image, train_labels),_ = tf.keras.datasets.mnist.load_data()
+
+# 对图片进行预处理
+train_image = tf.expand_dims(train_image,-1) # 扩增维度
+#print(train_image.shape)  #(60000, 28, 28, 1)
+#print(train_image[0])    #输出第一个element
+
+
+# 计算梯度必须使用浮点型
+# 归一化
+train_image = tf.cast(train_image/255, tf.float32)
+#print(train_image.shape)  #(60000, 28, 28, 1)
+#print(train_image[0])    #输出第一个element
+#print(train_image[0].shape)    #输出第一个element的维度   (28, 28, 1)
+
+
+# 对label进行处理
+train_labels = tf.cast(train_labels, tf.int64)
+
+# print(train_labels.shape)  #(60000,)
+# print(train_labels[0])    #输出第一个element  tf.Tensor(5, shape=(), dtype=int64)
+# print(train_labels[0].shape)    #输出第一个element的维度 ()
+
+dataset = tf.data.Dataset.from_tensor_slices((train_image, train_labels))
+
+# 连续、批次
+dataset = dataset.shuffle(10000).batch(32)
+
+"""
+这段代码是使用 TensorFlow 库中的tf.data.Dataset模块创建一个数据集，并对数据进行随机打乱和批处理。
+首先，使用tf.data.Dataset.from_tensor_slices函数将训练图像和训练标签转换为tf.data.Dataset对象。
+该函数接受一个元组作为参数，其中第一个元素是训练图像，第二个元素是训练标签。
+接下来，使用dataset.shuffle(10000)函数对数据集进行随机打乱。其中参数 10000 表示打乱的次数，可以根据需要进行调整。
+最后，使用dataset.batch(32)函数对数据集进行批处理。其中参数 32 表示每批数据的大小，即每次取出 32 个样本进行处理。
+这样做的目的是为了提高计算效率，减少内存占用。
+需要注意的是，tf.data.Dataset模块是 TensorFlow 2.0 中的新特性，它提供了一种高效的数据处理方式，可以提高模型的训练效率和精度。
+"""
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(16,[3,3],activation="relu",input_shape=(None,None,1)),
+    tf.keras.layers.Conv2D(32,[3,3],activation="relu"),
+    tf.keras.layers.GlobalMaxPool2D(),
+    tf.keras.layers.Dense(10)
+])
+
+
+""" 
+这段代码定义了一个使用卷积神经网络（CNN）架构的 Keras 模型。
+模型的输入是一个三维张量，形状为（None, None, 1），其中 None 表示输入的图像大小可以是任意的。模型的输出是一个十维的向量，表示预测的结果。
+模型由以下几个层组成：
+tf.keras.layers.Conv2D：这是一个卷积层，使用 16 个大小为 3x3 的卷积核，激活函数为 ReLU。
+tf.keras.layers.Conv2D：这是另一个卷积层，使用 32 个大小为 3x3 的卷积核，激活函数为 ReLU。
+tf.keras.layers.GlobalMaxPool2D：这是一个全局最大池化层，用于减少特征图的维数。
+tf.keras.layers.Dense：这是一个全连接层，使用 10 个神经元，用于对特征图进行分类。
+该模型可以用于图像分类任务，例如手写数字识别、图像分类等。
+ """
+
+optimizer = tf.keras.optimizers.Adam()   # 优化器，默认Adam参数
+
+loss_func = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)   #多分类常用的损失函数
+
+train_loss = tf.keras.metrics.Mean("train_loss")
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy('train_accuracy')    
+#定义了两个tf.keras.metrics，分别是train_loss和train_accuracy。这两个指标用于记录训练过程中的损失和准确率。
+
+
+#定义了一个train_step函数，用于执行训练的单个步骤。该函数使用tf.GradientTape计算损失函数的梯度，并使用optimizer优化器更新模型的参数
+def train_step(model,images,labels):
+    with tf.GradientTape() as t:
+        pred = model(images)
+        loss_step = loss_func(labels,pred)
+    grads = t.gradient(loss_step, model.trainable_variables)
+    optimizer.apply_gradients(zip(grads,model.trainable_variables))
+    train_loss(loss_step)
+    train_accuracy(labels,pred)
+
+"""    
+具体地，第一行代码使用tf.GradientTape计算损失函数的梯度，其中t是一个tf.GradientTape对象。然后，使用t.gradient
+函数计算损失函数相对于模型中所有可训练变量的梯度。这些梯度存储在变量grads中。
+第二行代码使用optimizer优化器应用梯度更新模型的权重参数。optimizer.apply_gradients函数接收两个参数：
+梯度变量和模型中的可训练变量。这里，我们使用zip函数将grads和模型中的可训练变量打包在一起，以便optimizer可以一次性更新所有的权重参数。
+通过不断地执行训练步骤，优化器将根据梯度更新模型的权重参数，以最小化损失函数，提高模型的性能。 
+"""
+
+def train():
+    for epoch in range(10):     # 训练10个epoch
+        for (batch,(images,labels)) in enumerate(dataset):
+            train_step(model,images,labels)
+        print("Epoch{} loss is {},accuracy is {}".format(epoch,
+                                                         train_loss.result(),
+                                                         train_accuracy.result()))
+        # 对于下一步进行重置
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+
+train()        
